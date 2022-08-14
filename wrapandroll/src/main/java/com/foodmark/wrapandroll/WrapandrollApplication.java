d2034c67186f0d1c6d4eacbc59b445fb2b319b76
Balaji Dontul <Balaji.Dontul@lntinfotech.com>
2022-08-14 21:32:01 +0530

    first commit

---
+++ wrapandroll/src/main/java/com/foodmark/wrapandroll/WrapandrollApplication.java
@@ -0,0 +1,13 @@
+package com.foodmark.wrapandroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrapandrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrapandrollApplication.class, args);
+	}
+
+}
---
+++ wrapandroll/src/test/java/com/foodmark/wrapandroll/WrapandrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodmark.wrapandroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrapandrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
